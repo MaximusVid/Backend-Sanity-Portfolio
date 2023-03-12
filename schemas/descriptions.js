da666137eb1ca6eb4442662c6a7ccc9d73dc7d54
MaximusVid <davidpermana47@gmail.com>
2023-03-12 17:31:17 +0700

    first commit

---
+++ schemas/descriptions.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'descriptions',
+  title: 'Descriptions',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    },
+    {
+      name: 'imageurl',
+      title: 'ImgUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+  ],
+}
