47db36d0a1f89f98e06c87ef764a9ac9ca3af72f
David Permana <75023414+MaximusVid@users.noreply.github.com>
2023-03-15 10:45:35 +0700

    Update sanity.cli.js

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'ylczjv01',
+    projectId: '{projectID}',
     dataset: 'production'
   }
 })
